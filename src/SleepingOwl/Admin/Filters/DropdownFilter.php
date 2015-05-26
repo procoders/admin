6963545720d9e0a1877e2a26472128674d16fe05
ivanoff4egs <ivanoff4eg.s@gmail.com>
2015-05-26 12:44:04 +0300

    * removed dependencies from filter

--- src/SleepingOwl/Admin/Filters/DropdownFilter.php
+++ src/SleepingOwl/Admin/Filters/DropdownFilter.php
@@ -4,29 +4,9 @@ namespace SleepingOwl\Admin\Filters;
 
 Class DropdownFilter extends BaseCustomFilter {
 
-
-    protected $depsOn = [];
-
-    protected $depsPresent = false;
-
-    public function __construct($depsOn = null, $value = null)
-    {
-        //dd($depsOn);
-        if (isset($depsOn) && isset($value)) {
-            $this->depsOn['depsOn'] = $depsOn;
-            $this->depsOn['value'] = $value;
-            $this->depsPresent = true;
-        }
-
-    }
-
     public function getOptions() {
         $repository = $this->model;
-        if ($this->depsPresent) {
-            return $repository::getOptionsList($this->depsOn['depsOn'], $this->depsOn['value']);
-        } else {
-            return $repository::getOptionsList();
-        }
+        return $repository::getOptionsList();
     }
 
     public function getValue() {
