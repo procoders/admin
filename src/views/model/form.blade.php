afcb2210fd8a1dca7e66c83e51ebefb1681d38b5
ivanoff4egs <ivanoff4eg.s@gmail.com>
2015-06-02 17:38:44 +0300

    OH-49 #time 2h 5 Mb upload file size restriction.

--- src/views/model/form.blade.php
+++ src/views/model/form.blade.php
@@ -11,4 +11,21 @@
 			{!! $form->render() !!}
 		</div>
 	</div>
+    <script>
+        var myFile = document.getElementById("teq");
+        if(myFile) {
+            myFile.addEventListener('change', function() {
+                var max_filesize = 5242880;
+                if (this.files[0].size > max_filesize) {
+                    $(":submit").attr('disabled', true);
+                    $('#filesize_error').remove();
+                    $('.clearfix').append('<div style="color: red; clear: both" id="filesize_error">File is too large. File size could not be more than 5 Mb.</div>');
+                } else {
+                    $('#filesize_error').remove();
+                    $(":submit").attr('disabled', false);
+                }
+            });
+        }
+    </script>
+
 @stop
