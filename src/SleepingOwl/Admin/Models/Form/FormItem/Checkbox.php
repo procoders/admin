f20e4edf4c82211ae5fe242baabe362dc04d68a0
ivanoff4egs <ivanoff4eg.s@gmail.com>
2015-06-02 19:47:27 +0300

    * using customCheckbox in the app

--- src/SleepingOwl/Admin/Models/Form/FormItem/Checkbox.php
+++ src/SleepingOwl/Admin/Models/Form/FormItem/Checkbox.php
@@ -4,9 +4,6 @@ class Checkbox extends BaseFormItem
 {
 	public function render()
 	{
-		if ($this->getName() == 'active') {
-            $this->attributes['checked'] = "checked";
-        }
         return $this->formBuilder->checkboxGroup($this->name, $this->label, $this->getValueFromForm(), $this->attributes);
 	}
 } 
