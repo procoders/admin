086e262653398d02f20f3477dce41ae880aff2de
buchkovsky <listat88@gmail.com>
2015-06-11 12:31:53 +0300

    OH-87 add actions icons

--- src/views/_partials/form_elements/actions.blade.php
+++ src/views/_partials/form_elements/actions.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group">
     <div style="border: 0; text-align: right;">
         <a href="{{$cancelUrl}}">
-        <button type="button" class="btn btn-white m-r-5 m-b-5">{{ Lang::get('admin::lang.table.cancel')}}</button>
+        <button type="button" class="btn btn-white m-r-5 m-b-5"><i class="fa fa-undo"></i>&nbsp;&nbsp;{{ Lang::get('admin::lang.table.cancel')}}</button>
         </a>
-        <button type="submit" class="btn btn-success m-r-5 m-b-5">{{Lang::get('admin::lang.table.save')}}</button>
+        <button type="submit" class="btn btn-success m-r-5 m-b-5"><i class="fa fa-floppy-o"></i>&nbsp;&nbsp;{{Lang::get('admin::lang.table.save')}}</button>
     </div>
 </div>
