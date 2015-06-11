b17bf12727da3eb185b51c8869d1014fb250eadd
buchkovsky <listat88@gmail.com>
2015-06-11 12:28:35 +0300

    OH-87 Fix cancel url

--- src/views/_partials/form_elements/actions.blade.php
+++ src/views/_partials/form_elements/actions.blade.php
@@ -1,6 +1,8 @@
 <div class="form-group">
     <div style="border: 0; text-align: right;">
+        <a href="{{$cancelUrl}}">
         <button type="button" class="btn btn-white m-r-5 m-b-5">{{ Lang::get('admin::lang.table.cancel')}}</button>
+        </a>
         <button type="submit" class="btn btn-success m-r-5 m-b-5">{{Lang::get('admin::lang.table.save')}}</button>
     </div>
 </div>
