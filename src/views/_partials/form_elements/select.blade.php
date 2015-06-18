6f78427289f5e62f9c23b4c475cbd9f106a53912
buchkovsky <listat88@gmail.com>
2015-06-18 15:45:27 +0300

    OH-87 fix dropdown selected values

--- src/views/_partials/form_elements/select.blade.php
+++ src/views/_partials/form_elements/select.blade.php
@@ -3,7 +3,7 @@
     <div class="col-md-10">
         <select class="form-control selectpicker" data-size="10" data-live-search="true" data-style="btn-white" name="{{$name}}" id="{{$id}}">
             @foreach ($options as $option => $optionTitle)
-                <option value="{{$option}}">{{$optionTitle}}</option>
+                <option value="{{$option}}" @if ($option == $value) selected="selected" @endif>{{$optionTitle}}</option>
             @endforeach
         </select>
     </div>
