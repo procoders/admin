1a6c7b16fb996ff449724b7cba3128e549e27919
buchkovsky <dmitry.buchkovsky@iceshop.nl>
2015-06-25 18:55:51 +0300

    OH-87 add expand, close functionality to the panels

--- src/views/model/form_group.blade.php
+++ src/views/model/form_group.blade.php
@@ -8,7 +8,12 @@
 
     <div class="panel panel-inverse">
         <div class="panel-heading">
-            {!! $group->getDescription() !!}
+            <div class="panel-heading-btn">
+                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand" data-original-title="" title=""><i class="fa fa-expand"></i></a>
+                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+                <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+            </div>
+            <h4 class="panel-title">{!! $group->getDescription() !!}</h4>
         </div>
         <div class="panel-body">
             @foreach($items as $item)
